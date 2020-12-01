f6f47c7055af6f7e3cf17e5eea3c680b6d7c85ac
Richard Lindsay <richard.lindsay@argos.co.uk>
2020-12-01 17:43:16 +0000

    Added christmas tree

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Advent Of Code 2020
-Another year, another advent of code, another 5 days before I give up. 
+Another year, another advent of code, another brute force solution, another 5 days before I give up. 
 
- ![](https://media.giphy.com/media/UChPEs9ghFo2s/giphy.gif)
+![](https://media.giphy.com/media/UChPEs9ghFo2s/giphy.gif)
--- src/main/kotlin/dev/richardlindsay/adventOfCode/AdventOfCode.kt
+++ src/main/kotlin/dev/richardlindsay/adventOfCode/AdventOfCode.kt
@@ -1,4 +1,31 @@
 package dev.richardlindsay.adventOfCode
 
+fun main(){
+
+    val baseLength = 100
+
+    var starString = ""
+
+    for (i in 0 .. baseLength/2 + 1) starString = starString.plus(" ")
+    starString = starString.plus("*")
+
+    val layers: MutableList<String> = mutableListOf(starString)
+
+    for (i in 0 .. baseLength / 2) {
+        var layerString = ""
+
+        for (j in 0 .. baseLength/2 - i) layerString = layerString.plus(" ")
+        for (j in 0 .. i ) layerString = layerString.plus("/")
+        layerString = layerString.plus("|")
+        for (j in 0 .. i ) layerString = layerString.plus("""\""")
+        layers.add(layerString)
+    }
+
+    layers.forEach {
+        println(it)
+    }
+}
+
 class AdventOfCode {
+
 }
